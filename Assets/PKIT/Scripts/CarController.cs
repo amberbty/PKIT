2af0d6fcfb342c9a3276989f9b6d2e306c779d81
Zeinab Bagheri Fard <145973209+ZeinabBF@users.noreply.github.com>
2024-10-16 16:14:05 +0200

    SteeringWeelTurning

--- Assets/PKIT/Scripts/CarController.cs
+++ Assets/PKIT/Scripts/CarController.cs
@@ -10,10 +10,16 @@ public class CarController : MonoBehaviour
     public float horizontalInput;
     public float forwardInput;
 
+    public Transform steeringWheel;
+    public float steeringLimitAngle = 100f;
+    public float steeringRotationSpeed = 60f;
+
+    private Rigidbody carRb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        carRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -21,9 +27,30 @@ public class CarController : MonoBehaviour
     {
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
-        // Move the vehicle forward based on vertical input
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+
+        UpdateSteerWheel(horizontalInput);
+
+        if(forwardInput != 0f)
+        {
+            // Move the vehicle forward based on vertical input
+            //carRb.AddForce(transform.forward * Time.deltaTime * speed * forwardInput, ForceMode.Impulse);
+            transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        }
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
 
+    }
+
+    public void UpdateSteerWheel(float horizontalInput)
+    {
+        if (horizontalInput != 0f)
+        {
+            steeringWheel.Rotate(Vector3.forward, -horizontalInput * steeringRotationSpeed * Time.deltaTime);
+        }
+        if (horizontalInput == 0f && steeringWheel.localEulerAngles.z > 5f)
+        {
+            steeringWheel.localEulerAngles = new Vector3(steeringWheel.localEulerAngles.x,
+                                                        steeringWheel.localEulerAngles.y,
+                                                        0f);
+        }
     }
 }
